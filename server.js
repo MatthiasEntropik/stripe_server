0e6d8be8b3aafebae0ca7bef39f95e8505dcdfda
MatthiasEntropik <matthias@entropik.digital>
2022-03-16 11:52:07 +0400

    update prix

--- server.js
+++ server.js
@@ -7,21 +7,20 @@ app.use(express.static("."));
 app.use(express.json());
 // An endpoint for your checkout 
 app.post("/checkout", async (req, res) => {
-    console.log(res)
     // Create or retrieve the Stripe Customer object associated with your user.
     let customer = await stripe.customers.create(); // This example just creates a new Customer every time
 
     // Create an ephemeral key for the Customer; this allows the app to display saved payment methods and save new ones
     const ephemeralKey = await stripe.ephemeralKeys.create(
         { customer: customer.id },
-        { apiVersion: '2020-08-27' }
+        { apiVersion: '2020-08-27' },
     );
 
     // Create a PaymentIntent with the payment amount, currency, and customer
     const paymentIntent = await stripe.paymentIntents.create({
-        amount: 973,
-        currency: "usd",
-        customer: customer.id
+        amount: req.body.price,
+        currency: "eur",
+        customer: customer.id,
     });
 
     // Send the object keys to the client
